a61baa8dff7343163b14d8d6f8353615b5dd0b09
Mirabell Büscher <mirabell.buescher@web.de>
2022-04-21 14:33:27 +0200

    Initial commit

---
+++ test/comp_graphics_lib/core_test.clj
@@ -0,0 +1,7 @@
+(ns comp-graphics-lib.core-test
+  (:require [clojure.test :refer :all]
+            [comp-graphics-lib.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
