9b82d5e7150452b2b84c37a6b80bda22538df19b
Mirabell Büscher <mirabell.buescher@web.de>
2023-02-19 15:40:51 +0100

    implement one-round forest fire and clean up

--- src/comp_graphics_lib/texture-generation/forest-fire.clj
+++ src/comp_graphics_lib/texture-generation/forest-fire.clj
@@ -13,21 +13,25 @@
 ;; -> 2|1 -> 1|0
 ;; -> 0|1 -> 0|0
 
+(ns comp-graphics-lib.texture-generation.forest-fire
+  (:require
+    [comp-graphics-lib.texture-generation.map-utils :as map-utils])
+  (:import )
+  )
+
+
 ;; configurations ========================================
 
-(def fire-probability 0.005)
-(def forest-probability 0.01)
+(def fire-probability 0.05)
+(def forest-probability 0.1)
 (def tree 2)
 (def fire 1)
 (def barren 0)
-(def mat [[tree tree tree]
+(def mat [[tree tree tree] 
           [tree fire tree]
           [tree barren tree]])
 
 ;; code ==================================================
-
-;; TESTS
-mat ; test
 
 (defn burn?
   "Returns true if the cell should start burning.
@@ -49,31 +53,8 @@ mat ; test
 ;; TESTS
 (grow? 0 forest-probability) ; test 
 
-(defn cell-exists?
-  "Returns true if the cell index exists in the map matrix.
-   Returns false otherwise."
-  [mat row col]
-  (and (contains? mat row) (contains? (-> mat (nth row)) col)))
-;; TESTS
-(cell-exists? mat 2 2) ; T
-(cell-exists? mat 2 3) ; F
-(cell-exists? mat 3 2) ; F 
-(cell-exists? mat 0 0) ; T
-(cell-exists? mat -1 0) ; F
-(cell-exists? mat 0 -1) ; F
-
-(defn get-cell
-  "Returns the content of a map cell at a given index.
-   Throws an IndexOutOfBounds exception if the index is invalid"
-  [mat row col]
-  (-> mat (nth row) (nth col)))
-;; TESTS
-(get-cell mat 2 2) ; 2
-(get-cell mat -1 0) ; IndexOutOfBounds
-(get-cell mat 0 -1) ; IndexOutOfBounds
-
 (defn on-fire? [cell]
-  (= cell 1))
+  (= cell fire))
 ;; TESTS
 (on-fire? 0) ; F
 (on-fire? 1) ; T
@@ -99,87 +80,35 @@ mat ; test
 (contains-fire? [[0 0 0]
                  [0 0 2]]) ; F
 
-(defn get-data-dict [mat row col]
-  (try {:row row :col col :val (get-cell mat row col)}
-       (catch Exception _ nil)))
-;; TESTS
-(get-data-dict mat 0 0) ; dict
-(get-data-dict mat -1 0) ; nil
-
-(defn get-neighbours
-  "Returns a dictionary of data dictionaries of all neighbours for a specific cell.
-   Each cell has eight neighbours (except border cells):
-   1 2 3 
-   4 x 5
-   6 7 8"
-  [mat row col]
-  {:1 (get-data-dict mat (dec row) (dec col))
-   :2 (get-data-dict mat (dec row) col)
-   :3 (get-data-dict mat (dec row) (inc col))
-   :4 (get-data-dict mat row (dec col))
-   :5 (get-data-dict mat row (inc col))
-   :6 (get-data-dict mat (inc row) (dec col))
-   :7 (get-data-dict mat (inc row) col)
-   :8 (get-data-dict mat (inc row) (inc col))})
-;; TESTS
-(def neighbourmap  [[1 2 3]
-                    [4 0 5]
-                    [6 7 8]])
-(get-neighbours neighbourmap 1 1) ; valid: all
-(get-neighbours neighbourmap 0 0) ; valid: 5,7,8
-
-(def neighb-keys (list :1 :2 :3 :4 :5 :6 :7 :8))
-
-(defn get-neighbour-of
-  "Returns the data-dictionary {row, column, value} of a neighbor at a specific position.
-   Returns nil if the neighbour position is invalid."
-  [mat row col neighbour]
-  ((get-neighbours mat row col) neighbour))
-;; TESTS
-(get-neighbour-of neighbourmap 1 1 :1) ; :val=1
-(get-neighbour-of neighbourmap 1 1 :2) ; :val=2
-(get-neighbour-of neighbourmap 1 1 :3) ; :val=3
-(get-neighbour-of neighbourmap 1 1 :4) ; :val=4
-(get-neighbour-of neighbourmap 1 1 :5) ; :val=5
-(get-neighbour-of neighbourmap 1 1 :6) ; :val=6
-(get-neighbour-of neighbourmap 1 1 :7) ; :val=7 
-(get-neighbour-of neighbourmap 1 1 :8) ; :val=8
-(get-neighbour-of neighbourmap 0 0 :1) ; nil
-
-
 (defn neighbour-on-fire? 
   "Returns true if the neighbour cell of the given key is on fire."
   [mat row col neighb]
-  (if (nil? ((get-neighbours mat row col) neighb))
+  (if (nil? ((map-utils/get-neighbours mat row col) neighb))
     false
-    (= (((get-neighbours mat row col) neighb) :val) fire)))
+    (= (((map-utils/get-neighbours mat row col) neighb) :val) fire)))
 ;; TESTS
 (neighbour-on-fire? mat 0 0 :5) ; F
 (neighbour-on-fire? mat 0 0 :8) ; T
 (neighbour-on-fire? mat 0 0 :1) ; does not exist; F
 
-
-(defn get-next-index 
-  "Iterates row by row and left to right over the map."
-  [mat row col]
-  (if (cell-exists? mat row (inc col))
-    (get-data-dict mat row (inc col)) 
-    (if (cell-exists? mat (inc row) 0)
-      (get-data-dict mat (inc row) 0) 
-      nil))) 
+(defn has-next-index?
+  "Returns true if get-next-index can find a next cell"
+  [mat row col] 
+  (not (nil? (map-utils/get-next-index mat row col))
+  ))
 ;; TESTS
-(get-next-index mat 0 0) ; mat 0 1
-(get-next-index mat 0 2) ; mat 1 0
-(get-next-index mat 2 2) ; nil
+(has-next-index? mat 0 0) ; true
+(has-next-index? mat 1 2) ; true
+(has-next-index? mat 2 2) ; false
 
 
-(defn neighbours-on-fire?
+(defn how-many-neighbours-on-fire?
  "Returns the number of fires in the neighbour cells.
   It also works for indices out of scope. This may be useful for algorithms that extend the map/texture area." 
   [mat row col]
   (if (contains-fire? mat)
    (loop [i 0, fire-sum 0]
-     (if (neighbour-on-fire? mat row col (nth neighb-keys i))
+     (if (neighbour-on-fire? mat row col (nth map-utils/neighb-keys i))
        (if (< i 7)
          (recur (inc i) (inc fire-sum))
          fire-sum)
@@ -191,23 +120,78 @@ mat ; test
 (def test-mat [[1 1 1]
                [1 0 2]
                [1 2 0]])
-(neighbours-on-fire? test-mat -1 1) ; 2
-(neighbours-on-fire? test-mat 2 2) ; 0
-(neighbours-on-fire? test-mat 1 1) ; 5
+(how-many-neighbours-on-fire? test-mat -1 1) ; 2
+(how-many-neighbours-on-fire? test-mat 2 2) ; 0
+(how-many-neighbours-on-fire? test-mat 1 1) ; 5
 
 ;; 0 -> 0 or 2
 ;; 1 -> 0
 ;; 2 -> 2 or 1
-(defn transform-cell 
+(defn next-cell-value 
   "Returns the value of the cell of the next map iteration"
   [mat row col]
-  (if (= (get-cell mat row col) 1)
-    barren
-    (if (burn? (get-cell mat row col) fire-probability)
-      fire
-      (if (grow? (get-cell mat row col) forest-probability)
-        tree
-        (get-cell mat row col)))))
+  (if (= (map-utils/get-cell mat row col) fire)
+    barren ; if burning then barren
+    (if (burn? (map-utils/get-cell mat row col) fire-probability)
+      fire ; else if probably start to burn (if tree inclusive)
+      (if (grow? (map-utils/get-cell mat row col) forest-probability)
+        tree ; else if probably start to grow (if barren inclusive)
+        (map-utils/get-cell mat row col)))) ; else just stay the same
+  ) 
+;; TESTS
+(next-cell-value test-mat 0 0) ; 0
+(next-cell-value test-mat 1 1) ; 0 or 2
+(next-cell-value test-mat 2 1) ; 1 or 2
+
+
+(defn transform-cell-in-mat
+  "Transformes one cell in the given matrix"
+  [mat row col] 
+  (assoc mat row (assoc (-> mat (nth row)) col (next-cell-value mat row col)))
+  ) ; assoc=replace vector index new_val
+;; TESTS
+(transform-cell-in-mat test-mat 0 0) ; [0 1 1] [1 0 2] [1 2 0]
+
+;; (defn transform-mat
+;;   [mat]
+;;   (for [row mat]
+;;    (for [cell row]
+;;     (transform-cell-in-mat ))))
+
+; better recursive
+
+(defn transform-mat-recur
+  [matrix first_row first_col]
+  (loop [mat matrix 
+         row first_row 
+         col first_col]
+       (if (has-next-index? mat row col)
+         (recur (transform-cell-in-mat mat row col) ;; transform current cell and get new mat
+                ((map-utils/get-next-index mat row col) :row) ;; next row
+                ((map-utils/get-next-index mat row col) :col)) ;; next col) 
+         (transform-cell-in-mat mat row col);; last cell
+                )
+  ))
+
+(defn forest-fire-one-round 
+  [matrix]
+  (transform-mat-recur matrix 0 0))
+
+;; TESTS
+(def big-test-mat
+  [[2 2 2 2 2 2 2 2 2 2]
+   [2 2 1 2 2 2 2 2 2 2]
+   [2 2 2 2 2 2 2 0 2 2]
+   [2 0 0 2 2 2 2 2 2 2]
+   [2 0 0 2 2 2 2 2 2 2]
+   [2 2 2 2 1 2 2 2 2 2]
+   [2 2 2 2 2 2 2 2 2 2]
+   [2 0 0 2 2 2 2 2 2 2]
+   [2 2 0 2 2 2 1 2 2 2]
+   [2 2 2 2 2 2 2 2 2 2]])
+(forest-fire-one-round test-mat) ; [[0 0 0] [0 0/2 2] [0 2 0/2]]
+(forest-fire-one-round big-test-mat)
+
 
 ;; TODO meta goal
 ;; # pure function
@@ -216,5 +200,8 @@ mat ; test
 
 ;; TODO meta goal
 ;; non-pure because of random|probabilistic behaviour
-(defn forest-fire [input_matrix, forest-probability, fire-probability] ())
+(defn run-forest-fire 
+  [matrix, forest-probability, fire-probability, number-of-rounds] 
+  ()
+  )
 
