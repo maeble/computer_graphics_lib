725fc118986aff88674dad4da2475397366bbc7a
Mirabell Büscher <mirabell.buescher@web.de>
2023-02-22 19:59:51 +0100

    add docs and ui for color texture map

--- src/comp_graphics_lib/color-generation/text-color-transformation.clj
+++ src/comp_graphics_lib/color-generation/text-color-transformation.clj
@@ -1,40 +1,54 @@
 (ns comp-graphics-lib.color-generation.text-color-transformation)
 
 
-(defn char-to-ascii [c]
+(defn char-to-color-number 
+  "Transforms a char to an ascii value. It does not just take the ascii integer value associated with the char, 
+   but tries to map chars a litte bit more randomly to the rgb color numeric range (0:255)."
+  [c]
   (let [color-range 256
         modulo-cut 111]
     (int (* (mod (int c) modulo-cut) (/ (dec color-range) (dec modulo-cut))))))
-; at first I set the modulo to 256 because it is both the ascii and the color range. 
+; at first I set the modulo-cut to 256 because it is both the ascii and the color range. 
 ; but because all texts would average now ton appriximately the same color in different shades,
 ; I decided to change the modulo border to approximately the center of the small characters and scale it up
 ; now, the behaviour is better, but of course not an optimal, randomized distribution. 
 ; For this, I would need to hash the string. But this would make the algorithm much simpler and I would not 
-; be able to try all this methods out.
+; be able to try out all this functional programming techniques
 
-(defn take-pruned-average [& values]
-  (let [all (apply concat values)]
+(defn take-pruned-average 
+  "Takes the average of all values and casts it to an integer."
+  [& value-list]
+  (let [all (apply concat value-list)]
     (int (/ (reduce + all) (count all))))) ; for the + operation, apply and reduce are equivalent btw 
 
-(defn transpose [coll]
+(defn transpose 
+  "Transposes a matrix."
+  [coll]
   (apply map vector coll))
 
-(defn round-up [d]
+(defn round-up 
+  "Always rounds up decimal values."
+  [d]
   (let [delta (- d (int d))]
     (if (< delta 0)
       (int d)
       (inc (int d)))))
 
-(defn take-data [n data]
+
+(defn take-data
+  "If possible, takes n items from data. 
+   If there are not enough values in data, it repeats the last value for filling up the output vector."
+  [n data]
   (let [token (vec (take n data))]
     (if (< (count token) n)
       (vec (concat token (vec (repeat (- n (count token)) (last token)))))
-      token
-      )
-    )
-  )
+      token)))
 
-(defn reshape [v x y] 
+(defn reshape 
+  "Reshapes a flatten, non-empty vector v. 
+   The items per row are defined by the integer x.
+   The number of rows is then determinated by the number of items in v."
+  [v x] 
   (loop [data v
          resmat []]
     (let [remaining (take-last (- (count data) x) data)
@@ -44,26 +58,34 @@
         iter-res))))
 
 
-(defn reshape-to-texture-map [rgb-list]
+(defn reshape-to-texture-map 
+  "Reshape the rgb-list to a matrix that is as square as possible."
+  [rgb-list]
   (let [x (round-up (Math/sqrt (count rgb-list)))]
-    (prn x)
-    (reshape rgb-list x x)))
+    (reshape rgb-list x)))
 
 
-(defn strings-to-asciis [& text] ;; lazy sequence with for loop
+(defn strings-to-color-nums 
+  "Text is transformed to a vector of color numbers."
+  [& text] 
   (let [entire-text (apply str text)]
-    (vec (for [c (char-array entire-text)] (char-to-ascii c)))) ;; doseq is less appropriate here as it returns nil, but for returns a lazy sequence 
+    (vec (for [c (char-array entire-text)] (char-to-color-number c)))) ;; doseq is less appropriate here as it returns nil, but for returns a lazy sequence 
   )
 
-(def average-color-values ; partial
+(def average-color-values ; adds to all color numbers the base color number 255
   (partial take-pruned-average [255]) ; with base value 255
   )
 
-(defn get-average-rgb [r-vals,g-vals,b-vals]
+(defn get-average-rgb 
+  "Calculates the average rgb-value."
+  [r-vals,g-vals,b-vals]
   [(average-color-values r-vals) (average-color-values g-vals) (average-color-values b-vals)])
 
-(defn asciis-color-transform [asciis, color-collection-fn]
-  (loop [seq asciis
+(defn number-color-transform 
+  "Takes 3 sequential numbers as one rgb triple. 
+   The color-collection-fn function defines, how all rgb triples shall be treated and returned (averaged, listed, etc)."
+  [numbers, color-collection-fn]
+  (loop [seq numbers
          reds []
          greens []
          blues []]
@@ -73,27 +95,31 @@
         (recur remaining (conj reds r) (conj greens g) (conj blues b)) ; tail recursion
         (color-collection-fn reds greens blues)))))
 
-(defn asciis-to-color
+(defn numbers-to-color
+  "Transforms color numbers to one rgb color."
   [coll]
-  (asciis-color-transform coll get-average-rgb))
+  (number-color-transform coll get-average-rgb))
 
 
-(defn get-rgb-values [r-vals,g-vals,b-vals]
+(defn get-rgb-values 
+  "Collects all rgb values in a list."
+  [r-vals,g-vals,b-vals]
   (map (fn [r g b] [r g b]) r-vals g-vals b-vals))
 
 (defn get-rgb-values' [r-vals,g-vals,b-vals] ;; equivalent
   (transpose [r-vals g-vals b-vals]))
 
 
-(defn asciis-to-colors
+(defn numbers-to-colors
+  "Transforms color numbers to a list of rgb colors."
   [coll]
-  (asciis-color-transform coll get-rgb-values))
+  (number-color-transform coll get-rgb-values))
 
-(def strings-to-color
-  (comp asciis-to-color strings-to-asciis)) ; composition 
+(def strings-to-color 
+  (comp numbers-to-color strings-to-color-nums)) ; composition 
 
 (def strings-to-colors
-  (comp asciis-to-colors strings-to-asciis))
+  (comp numbers-to-colors strings-to-color-nums)) ; composition 
 
 (def strings-to-texture-map
   (comp reshape-to-texture-map strings-to-colors)
---
+++ src/comp_graphics_lib/color-generation/text-to-color-ui.clj
@@ -0,0 +1,84 @@
+(ns comp-graphics-lib.color-generation.text-to-color-ui
+  (:require [cljfx.api :as fx]
+            [comp-graphics-lib.texture-generation.map-utils :as mu]
+            [comp-graphics-lib.color-generation.text-color-transformation :as text2color]
+            )
+  )
+
+(defn rgb-2-hexadezimal [rgb-vec]
+  (let [[r g b] rgb-vec]
+    (format "#%02x%02x%02x", r, g, b)))
+
+;; Define application state
+(defn get-state [window-title matrix on-click-generate-button]
+  (atom {:title window-title
+         :matrix matrix
+         :on-click-generate-button on-click-generate-button}))
+
+(defn get-rectangle [row col color]
+  {:fx/type :rectangle
+   :width 15
+   :height 15
+   :grid-pane/row (inc row)
+   :grid-pane/column (inc col)
+   :fill (rgb-2-hexadezimal color)
+   })
+
+;(get-rectangle 0 0 [0 0 0])
+
+
+(defn matrix-2-map [matrix]
+  (loop [mat matrix, row 0, col 0, result []]
+    (let [cell-value (mu/get-cell-value mat row col)
+          new-result (conj result (get-rectangle row col cell-value))]
+      (if (mu/has-next-index? mat row col)
+        (let [[next-row next-col] (mu/get-next-cell mat row col)] ;; seq destructuring
+          (recur mat next-row next-col new-result))
+        new-result))))
+
+
+(defn root [{:keys [title
+                    matrix
+                    on-click-generate-button
+                    ]}] ;; uses associative destructuring of the atom dictionary of *state
+  (println "root: create window, map, etc")
+  {:fx/type :stage
+   :showing true
+   :title title
+   :scene {:fx/type :scene
+           :root {:fx/type :v-box
+                  :alignment :center
+                  :spacing 10.0
+                  :children [{:fx/type :label
+                              :style {:-fx-text-fill :black, :-fx-font-weight :bold}
+                              :text ""}
+                             {:fx/type :label
+                              :style {:-fx-text-fill :black, :-fx-font-weight :bold}
+                              :text "    Interactive Text to Texture Generation    "}
+                             {:fx/type :label
+                              :text "Enter a text and generate a texture."}
+                             {:fx/type :button
+                              :text ">> Generate texture"
+                              :on-action on-click-generate-button} 
+                             {:fx/type :grid-pane
+                              :alignment :center
+                              :children (matrix-2-map  matrix)}
+                             {:fx/type :label
+                              :style {:-fx-text-fill :black, :-fx-font-weight :bold}
+                              :text ""}
+                             ]}}})
+
+(defn renderer []
+  (fx/create-renderer
+   :middleware (fx/wrap-map-desc assoc :fx/type root)))
+
+(defn show [*state renderer]
+  (fx/mount-renderer *state renderer))
+
+(def texture-mat (text2color/strings-to-texture-map "num is used to coerce a primitive Java number type such as int, float, long, double, etc., into its boxed version such as Float, Long, Double, etc. If given an existing boxed Number type, as opposed to a primitive number type, it will just return it as is."))
+
+
+;; RUN
+
+(show (get-state "Text to color map generation" texture-mat (fn [_] (prn "click"))) 
+      (renderer))
